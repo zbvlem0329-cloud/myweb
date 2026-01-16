e6f723cb3d573c98a40d3866dda53978afd8d807
DESKTOP-BNLKHI6\user1 <writingking94>
2026-01-16 11:48:42 +0900

    회원가입 유효성 검증

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log(123);
+function handleJoin() {
+    const userId = document.querySelector("input[name=userId]").value;
+    const userPw = document.querySelector("input[name=userPw]").value;
+    const userPw2 = document.querySelector("input[name=userPw2]").value;
+    const userNick = document.querySelector("input[name=userNick]").value;
+
+
+
+    //id
+    const idOk = /^[a-z0-9]{4,8}$/.test(userId);
+    if (!idOk) { 
+        alert("아이디 확인 바람")
+        return false; }
+    //pw
+    const pwOk1 = /^[0-9]{4,8}$/.test(userPw);
+    const pwOk2=userPw===userPw2;
+    const pwOk= pwOk1 && pwOk2;
+
+    if (!pwOk) { 
+        alert("비밀번호 확인 바람")
+        return false; }
+    //nick
+    const nickOk = /^[가-힣]{2,4}$/.test(userNick);
+    if (!nickOk) { 
+        alert("닉네임 확인 바람")
+        return false; }
+
+}
